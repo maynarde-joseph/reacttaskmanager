dc6204b826291f0e52b86a324809b917247b8242
MaynardeJoseph <maynarde.joseph@gmail.com>
2024-03-17 17:40:59 +1100

    small fix

--- app/(components)/RegisterForm.jsx
+++ app/(components)/RegisterForm.jsx
@@ -4,7 +4,6 @@
 import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import bg from "@/public/2.jpg";
 
 export default function RegisterForm() {
   const [name, setName] = useState("");
